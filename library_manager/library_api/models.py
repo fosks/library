b8f095f89884fdcd21f7397e6fb2ca3663fb6e22
Gustavo Shinji Inoue <gs.inoue@gmail.com>
2020-10-31 23:08:18 -0300

    Add new field to book model

--- library_manager/library_api/models.py
+++ library_manager/library_api/models.py
@@ -6,3 +6,4 @@ class BookData(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
     description = models.CharField(max_length=200, blank=False, default='')
     published = models.BooleanField(default=False)
+    submitted = models.BooleanField(default=False)
