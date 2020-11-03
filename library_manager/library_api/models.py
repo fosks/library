6769a4cbab39db7eb9d8965812bfe633609b24a3
Gustavo Shinji Inoue <gs.inoue@gmail.com>
2020-11-03 00:20:25 -0300

    Make fields optional in Data Model

--- library_manager/library_api/models.py
+++ library_manager/library_api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class BookData(models.Model):
     title = models.CharField(max_length=70, blank=False, default='')
-    author = models.CharField(max_length=70, blank=False, default='')
-    publisher = models.CharField(max_length=70, blank=False, default='')
-    summary = models.CharField(max_length=200, blank=False, default='')
-    release_date = models.DateField(default=date.today)
-    category = models.CharField(max_length=70, blank=False, default='')
+    author = models.CharField(max_length=70, blank=True, default='')
+    publisher = models.CharField(max_length=70, blank=True, default='')
+    summary = models.CharField(max_length=200, blank=True, default='')
+    release_date = models.DateField(blank=True, default=date.today)
+    category = models.CharField(max_length=70, blank=True, default='')
